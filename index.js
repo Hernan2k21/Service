a7f6dc4acf268796a13ec2d5502f22c400bd573d
Hernan2k21 <hernan@genosha.com.ar>
2022-08-04 10:55:16 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express"); 
+const {port} = require('./config/app.config')['server']
+const {logError, sendError, notFound} = require('./src/Middlewares')
+const setupLogger = require('./src/Helpers/setupLogger')
+const initDatabase = require('./src/Helpers/database')
+const app = express(); 
+
+
+app.use(
+    express.json({
+        limit: '50mb',
+    })
+);
+
+setupLogger(app)
+const routes = require('./src/Routes');
+
+app.get("/health", async (req, res) => { 
+    res.status(200).send('ok')
+}); 
+
+app.use('/v1', routes)
+app.use(logError)
+app.use(sendError)
+app.use(notFound)
+
+// initDatabase()
+app.listen(port, () => { 
+    console.log(`API is listening on port ${port}`); 
+});
